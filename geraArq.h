f8de38cba08ca58f6391b7badcc53d66ae6abac7
lazaroq11 <42515400+lazaroq11@users.noreply.github.com>
2020-03-10 17:51:11 -0300

    v1 ana_lex

---
+++ geraArq.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include "lexico.h"
+#include "token.h"
+#include "allocation.h"
+char saiArq(char *arqSaida){
+
+token t;
+
+
+
+FILE *Arq2;
+
+Arq2 = fopen("saida.txt","w");
+
+if(Arq2 == NULL){
+ 
+	printf("O arquivo não foi aberto/n");
+}
+   
+          
+else{
+	
+    
+	fprintf("saida.txt","%d",t.tipo);
+	fprintf("saida.txt","%s",t.lexema.c_str());
+	fprintf("saida.txt","%d",t.posicaoLex);
+	
+	fclose(Arq2); 
+	  } 
+
+
+}
---
+++ token.h
@@ -0,0 +1,52 @@
+/* 
+
+DISCIPLINA : FUNDAMENTOS DE COMPILADORES
+ALUNO: LÁZARO QUEIROZ DA SILVA
+EMAIL: lazaroq11@gmail.com
+TÍTULO : TOKEN.H
+
+*/
+
+
+#pragma once
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include "allocation.h"
+#include "lexico.h"
+#include "geraArq.h"
+void retornaToken(){ //rotina para retornar token
+	token t;
+	
+	 do{
+	 	
+	 	t = proxToken();
+	 	
+	 	
+	 	switch(t.tipo){ //Imprime na tela os tokens
+	 		
+	 		case 1: printf("Token = PALAVRA RESERVADA\n"); break;
+	 		case 2: printf("Token = TIPO\n"); break;
+	 		case 3: printf("Token = OPERADOR ARITMETICO\n"); break;
+	 		case 4: printf("Token = OPERADOR LOGICO\n");break;
+	 		case 5: printf("Token = IGUAL\n"); break;
+	 		case 6: printf("Token = ABRE PARENTESES\n"); break;
+	 		case 7: printf("Token = FECHA PARENTESES\n");break;
+	 		case 8: printf("Token = ENDLINE\n");break;
+	 		case 9: printf("Token = VIRGULA\n");break;
+	 		case 10:printf("Token = NUMERO\n");break;
+	 		case 11:printf("Token = IDENTIFICADOR\n");break;
+	 		case 12: printf("Token = FEOF\n");break;
+	 		case 13: printf("Token = ERRO\n");break;
+		 }
+		 
+		 printf("Lexema = %s\n",t.lexema);
+		 printf("Linha = %d\n\n\n",t.posicaoLex);
+	 }
+	 
+	 while(t.tipo != 12); //encerra no final da sentenca
+	 	
+		 
+		 delete [] vet; //libera memória
+	 
+}
